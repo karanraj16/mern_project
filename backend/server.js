1e9ec8633e4a80f4b2aad157d9a95f997233d70a
karanraj167 <karanraj.sankar16@gmail.com>
2025-11-28 16:04:58 +0530

    React app into frontend folder

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const errorHandler = require("./middleware/errorMiddleware");
 const path = require("path");
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 const app = express();
 
